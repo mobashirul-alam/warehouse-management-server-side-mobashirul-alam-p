d1cc9930f1d2c7122715a8786c88083ddb18b7a5
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-05-07 16:50:52 +0600

    products info displayed from database

--- index.js
+++ index.js
@@ -4,15 +4,33 @@ const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+require('dotenv').config();
+
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jed60.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const equipmentCollection = client.db('fitnessProEquipment').collection('product');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = equipmentCollection.find(query);
+            const results = await cursor.toArray();
+            res.send(results);
+        })
+    }
+    finally {
+
+    }
+};
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
